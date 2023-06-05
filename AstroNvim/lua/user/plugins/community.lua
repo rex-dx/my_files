2337fd1d0a0af847f21ba34d952b0dab9abbae20
rex-dx <130913178+rex-dx@users.noreply.github.com>
2023-06-05 07:11:54 -0500

    Add files via upload

---
+++ AstroNvim/lua/user/plugins/community.lua
@@ -0,0 +1,9 @@
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.colorscheme.nightfox", enabled = true },
+  { import = "astrocommunity.colorscheme.kanagawa", enabled = true },
+  { import = "astrocommunity.colorscheme.rose-pine" },
+  { import = "astrocommunity.colorscheme.dracula" },
+  { import = "astrocommunity.colorscheme.onigiri" },
+  { import = "astrocommunity.colorscheme.tokyonight" },
+}
